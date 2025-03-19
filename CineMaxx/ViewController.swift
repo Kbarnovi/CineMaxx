a16265eb2258602cff4c39b91b1b0fa42baeb718
kbarnovi <kbarnovi1@gmail.com>
2025-03-19 16:44:32 +0400

    Initial Commit

---
+++ CineMaxx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineMaxx
+//
+//  Created by Ketevan Barnovi on 19.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
