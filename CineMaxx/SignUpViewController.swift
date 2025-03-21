ce51a772426a688f03b4af4e029a2e7726adc586
kbarnovi <kbarnovi1@gmail.com>
2025-03-21 21:45:34 +0400

    updates

--- CineMaxx/SignUpViewController.swift
+++ CineMaxx/SignUpViewController.swift
@@ -1,13 +1,15 @@
-//
-//  SignUpViewController.swift
-//  CineMaxx
-//
-//  Created by Ketevan Barnovi on 19.03.25.
+////
+////  SignUpViewController.swift
+////  CineMaxx
+////
+////  Created by Ketevan Barnovi on 19.03.25.
+////
 //
 
 import UIKit
+import Security
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var surnameTextField: UITextField!
@@ -18,49 +20,124 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.becomeFirstResponder()
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.isSecureTextEntry = true
+        
+
+        [nameTextField, surnameTextField, emailTextField, passwordTextField, confirmPasswordTextField].forEach {
+            $0?.delegate = self
+            $0?.autocorrectionType = .no
+            $0?.spellCheckingType = .no
+        }
 
         setGradientBackground()
         
-        nameTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        surnameTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        emailTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        passwordTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        confirmPasswordTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        
-        
-        
-        
-        nameTextField.attributedPlaceholder = NSAttributedString(
-            string: "Name",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        )
-        
-        surnameTextField.attributedText = NSAttributedString(
-            string: "Surname",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        )
-        
-        emailTextField.attributedPlaceholder = NSAttributedString(
-            string: "Email",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        )
-        
-        passwordTextField.attributedPlaceholder = NSAttributedString(
-            string: "Password",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        )
-        
-        confirmPasswordTextField.attributedText = NSAttributedString(
-            string: "Confirm Password",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        )
+
+        [nameTextField, surnameTextField, emailTextField, passwordTextField, confirmPasswordTextField].forEach {
+            $0?.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+        }
+
+        setPlaceholderText()
+    }
+
+    func setPlaceholderText() {
+        let placeholderColor = UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)
+        nameTextField.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [NSAttributedString.Key.foregroundColor: placeholderColor])
+        surnameTextField.attributedPlaceholder = NSAttributedString(string: "Surname", attributes: [NSAttributedString.Key.foregroundColor: placeholderColor])
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: placeholderColor])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: placeholderColor])
+        confirmPasswordTextField.attributedPlaceholder = NSAttributedString(string: "Confirm Password", attributes: [NSAttributedString.Key.foregroundColor: placeholderColor])
     }
     
+    @IBAction func signUpTapped(_ sender: Any) {
+        let name = nameTextField.text ?? ""
+        let surname = surnameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let confirmPassword = confirmPasswordTextField.text ?? ""
+        
 
+        if name.isEmpty || surname.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty {
+            showAlert(message: "Please fill in all fields")
+            return
+        }
+        
+
+        if password != confirmPassword {
+            showAlert(message: "Passwords do not match")
+            return
+        }
+
+
+        if password.count < 8 {
+            showAlert(message: "Password must be at least 8 characters")
+            return
+        }
+        
+//        // Save email and password securely in Keychain
+//        saveToKeychain(email: email, password: password)
+//        
+//        // Navigate to Sign In screen
+//        navigateToSignIn()
+    }
     
+//    // Save email and password to Keychain
+//    func saveToKeychain(email: String, password: String) {
+//        let emailData = email.data(using: .utf8)!
+//        let passwordData = password.data(using: .utf8)!
+//        
+//        let emailQuery: [CFString: Any] = [
+//            kSecClass: kSecClassGenericPassword,
+//            kSecAttrAccount: "email",
+//            kSecValueData: emailData
+//        ]
+//        
+//        let passwordQuery: [CFString: Any] = [
+//            kSecClass: kSecClassGenericPassword,
+//            kSecAttrAccount: "password",
+//            kSecValueData: passwordData
+//        ]
+//        
+//        SecItemDelete(emailQuery as CFDictionary) // Remove old entry
+//        SecItemDelete(passwordQuery as CFDictionary) // Remove old entry
+//        
+//        SecItemAdd(emailQuery as CFDictionary, nil) // Add new email
+//        SecItemAdd(passwordQuery as CFDictionary, nil) // Add new password
+//    }
+    
+    // Show alert message
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // Handle return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let textFields: [UITextField] = [nameTextField, surnameTextField, emailTextField, passwordTextField, confirmPasswordTextField]
+        
+        if let index = textFields.firstIndex(of: textField), index < textFields.count - 1 {
+            textFields[index + 1].becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            signUpTapped(UIButton()) // Trigger sign up
+        }
+        return true
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let signInVC = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+            navigationController?.pushViewController(signInVC, animated: true)
+        }
+    }
+
+
     func setGradientBackground() {
-        let colorTop = UIColor(red: 132.0/255, green: 104.0/255, blue: 212.0/255, alpha: 1.0) .cgColor
-        let colorBottom = UIColor(red: 39.0/255, green: 16.0/255, blue: 107.0/255, alpha: 1.0) .cgColor
+        let colorTop = UIColor(red: 132.0/255, green: 104.0/255, blue: 212.0/255, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 39.0/255, green: 16.0/255, blue: 107.0/255, alpha: 1.0).cgColor
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop, colorBottom]
@@ -69,4 +146,14 @@ class SignUpViewController: UIViewController {
         
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
+    // Navigate to Sign In screen
+    func navigateToSignIn() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let signInVC = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+            navigationController?.pushViewController(signInVC, animated: true)
+            
+        }
+    }
 }
+
--- CineMaxx/ViewController.swift
+++ CineMaxx/ViewController.swift
@@ -7,33 +7,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         emailTextField.becomeFirstResponder()
         emailTextField.keyboardType = .emailAddress
         passwordTextField.isSecureTextEntry = true
+        
+        // დელეგატის მინიჭება
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        
+        emailTextField.autocorrectionType = .no
+        emailTextField.spellCheckingType = .no
+        passwordTextField.autocorrectionType = .no
+        passwordTextField.spellCheckingType = .no
+        
         
         setGradientBackground()
         
         emailTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
         passwordTextField.backgroundColor = UIColor.black.withAlphaComponent(0.2)
         
+        let placeholderColor = UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)
         emailTextField.attributedPlaceholder = NSAttributedString(
             string: "Email",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
+            attributes: [NSAttributedString.Key.foregroundColor: placeholderColor]
         )
-        
         passwordTextField.attributedPlaceholder = NSAttributedString(
             string: "Password",
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)]
-        ) 
+            attributes: [NSAttributedString.Key.foregroundColor: placeholderColor]
+        )
     }
-
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            textField.resignFirstResponder()
+            signUpTapped(UIButton())
+        }
+        return true
+    }
+    
+    @IBAction func signinTapped(_ sender: UIButton) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if email.isEmpty || password.isEmpty {
+            showAlert(message: "Please enter both email and password")
+            return
+        }
+        // სატესტო მონაცემები
+        let testEmail = "kbarnovi1@gmail.com"
+        let testPassword = "password123"
+        
+        // შემოწმება
+        if email == testEmail && password == testPassword {
+            MoveToHomePage()
+        } else {
+            showAlert(message: "Incorrect email or password")
+        }
+        print("Email: \(email), Password: \(password)")
+        
+        
+        
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+        // სტორიბორდიდან "SignUpViewController" დააბრუნებს და გადაგიყვანს ახალ ეკრანზე
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController {
+            navigationController?.pushViewController(signUpViewController, animated: true)
+        }
+    }
+    
+    func MoveToHomePage() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if let homePageVC = storyboard.instantiateViewController(withIdentifier: "HomePageViewController") as? HomePageViewController {
+            navigationController?.pushViewController(homePageVC, animated: true)
+        }
+    }
+    
     
     func setGradientBackground() {
         let colorTop = UIColor(red: 132.0/255, green: 104.0/255, blue: 212.0/255, alpha: 1.0) .cgColor
