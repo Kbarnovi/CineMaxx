ac489d24acd5a69b6c6c82938944f02c4840d821
kbarnovi <kbarnovi1@gmail.com>
2025-03-25 10:11:58 +0400

    morning update

---
+++ CineMaxx/Screens/HomePage/DetailControllerViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DetailControllerViewCell.swift
+//  CineMaxx
+//
+//  Created by Ketevan Barnovi on 25.03.25.
+//
+
+import UIKit
+
+class DetailControllerViewCell: UICollectionViewCell {
+    
+}
